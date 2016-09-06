c2a6d7df98250f7c74d89f25f48808c6f9986842
Axel Bock <mr.axel.bock@gmail.com>
2016-09-07 01:19:05 +0200

    add day1_quartiles.py

---
+++ 10-days-of-stats/day1_quartiles.py
@@ -0,0 +1,19 @@
+num = int(input())
+arr = sorted(list(map(int, input().split())))
+assert len(arr) == num
+
+def leftright(l):
+    llen = len(l)
+    return l[:(llen//2)], l[-(llen//2):]
+
+def median(l):
+    llen = len(l)
+    if llen % 2 == 1:
+        return l[llen//2]
+    else:
+        return (l[(llen//2)-1]+l[llen//2]) / 2
+
+left, right = leftright(arr)
+print(median(left))
+print(median(arr))
+print(median(right))

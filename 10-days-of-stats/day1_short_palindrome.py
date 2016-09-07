3b6dd6651605ece376a1cca99508b36148c47396
Axel Bock <mr.axel.bock@gmail.com>
2016-09-07 05:15:03 +0200

    rewrite short_palindrome, still no good

--- 10-days-of-stats/day1_short_palindrome.py
+++ 10-days-of-stats/day1_short_palindrome.py
@@ -1,20 +1,32 @@
-#!/usr/bin/python3
+from collections import defaultdict
 
-# let's try to not do string comparisons and maybe list indexing
-# is faster than string indexing
-s = list(map(ord, list(input())))
+# testset: s='kkkkkkz', result=15, with this we get 14.
 
+#s = list(map(ord, list(input())))
+#s = "apopohauugha"
+s = 'kkkkkkz'
 slen = len(s)
+
 found = 0
 
-# baseline optimization only (don't know if there is more possible)
-for a in range(0, slen-3):
-    for d in range(a+3, slen):
-        if not s[d] == s[a]: 
-            continue
-        for b in range(a+1, d-1):
-            for c in range(b+1, d):
-                if s[b] == s[c]:
-                    found += 1
+# there must be cause this timeouts.
+countdict = defaultdict(int)
+cd_start = 1
+a = 0
+for d in range(a + 3, slen):
+    if not s[d] == s[a]:
+        continue
+    else:
+        for idx in range(cd_start, d):
+            countdict[s[idx]] += 1
+        cd_start = d
+        for val in countdict.values():
+            val_tmp = val - 1
+            found += int((val_tmp / 2) * (val_tmp + 1))
+for a in range(0, slen - 3):
+    countdict[s[a]] -= 1
+    assert countdict[s[a]] >= 0
+    for val in countdict.values():
+        found += int((val / 2) * (val + 1))
 
-print(found % (10**9 + 7))
+print(found % (10 ** 9 + 7))
